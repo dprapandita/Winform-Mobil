c09a1329dbeb3eef1dbac5a8aec0c2a790328fc0
dprapandita@gmail.com <cybersecure751@gmail.com>
2026-05-03 20:44:31 +0700

    Fixing error

---
+++ Views/Dashboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Winform_Mobil.Views
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
