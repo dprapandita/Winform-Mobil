4b6a5ad1c1df66a3ec658798187547bd6cbd105c
dprapandita@gmail.com <cybersecure751@gmail.com>
2026-05-03 20:35:55 +0700

    Add project files.

---
+++ Models/Mobil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Winform_Mobil.Models
+{
+    public class Mobil
+    {
+        private string _merk;
+        private string _tipe;
+        private int _tahun;
+
+    }
+}
